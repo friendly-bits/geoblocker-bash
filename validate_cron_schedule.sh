5681397000c8e432a9ab24abf79e5b05cdb18ac0
blunderful-scripts <134004289+blunderful-scripts@users.noreply.github.com>
2023-05-26 21:39:34 +0300

    multiple improvements
    - better handle input that is missing parts of schedule expression
    - fixed indentation
    - improved console output messages

--- validate_cron_schedule.sh
+++ validate_cron_schedule.sh
@@ -8,61 +8,52 @@
 #    separated by commas (a,c,z), or an asterisk. Note that the step value notation of Vixie cron is not supported (e.g., 2-6/2).
 #
 #    Based on prior "verifycron" script circulating on the internets
-#    This is a simplified version, adapted to receive one cron schedule expression in a parameter.
+#    This is a simplified version, adapted to receive one cron schedule expression in an argument.
 
 
 #### Functions
 
-validNum()
-{
-  # return 0 if valid, 1 if not. Specify number and maxvalue as args
-  num=$1   max=$2
-
-  if [ "$num" = "X" ] ; then
-    return 0
-  elif [ ! -z $(echo $num | sed 's/[[:digit:]]//g') ] ; then
-    return 1
-  elif [ $num -lt 0 -o $num -gt $max ] ; then
-    return 1
-  else
-    return 0
-  fi
+validNum() {
+# return 0 if valid, 1 if not. Specify number and maxvalue as args
+	num=$1; max=$2
+	if [ "$num" = "X" ] ; then
+		return 0
+	elif [ ! -z $(echo $num | sed 's/[[:digit:]]//g') ] ; then
+		return 1
+	elif [ $num -lt 0 -o $num -gt $max ] ; then
+		return 1
+	else
+		return 0
+	fi
 }
 
-validDay()
-{
-  # return 0 if a valid dayname, 1 otherwise
-
-  case $(echo $1 | tr '[:upper:]' '[:lower:]') in
-    sun|mon|tue|wed|thu|fri|sat) return 0 ;;
-    X) return 0    ;; # special case - it's an "*"
-    *) return 1
-  esac
+validDay() {
+# return 0 if a valid dayname, 1 otherwise
+	case $(echo $1 | tr '[:upper:]' '[:lower:]') in
+		sun|mon|tue|wed|thu|fri|sat) return 0 ;;
+		X) return 0    ;; # special case - it's an "*"
+		*) return 1
+	esac
 }
 
-validMon()
-{
-  # return 0 if a valid month name, 1 otherwise
-
-   case $(echo $1 | tr '[:upper:]' '[:lower:]') in 
-     jan|feb|mar|apr|may|jun|jul|aug) return 0        ;;
-     sep|oct|nov|dec)             return 0        ;;
-     X) return 0 ;; # special case, it's an "*"
-     *) return 1    ;;
-   esac
+validMon() {
+# return 0 if a valid month name, 1 otherwise
+	case $(echo $1 | tr '[:upper:]' '[:lower:]') in
+		jan|feb|mar|apr|may|jun|jul|aug) return 0;;
+		sep|oct|nov|dec) return 0;;
+		X) return 0;; # special case, it's an "*"
+		*) return 1;;
+	esac
 }
 
-fixvars()
-{
-  # translate all '*' into 'X' to bypass shell expansion hassles
-  # save original as "sourceline" for error messages
+fixvars() {
+# translate all '*' into 'X' to bypass shell expansion hassles
 
-  sourceline="$min $hour $dom $mon $dow $extra"
-   min=$(echo "$min" | tr '*' 'X')
-   hour=$(echo "$hour" | tr '*' 'X')
-   dom=$(echo "$dom" | tr '*' 'X')
-   mon=$(echo "$mon" | tr '*' 'X')
-   dow=$(echo "$dow" | tr '*' 'X')
+	min=$(echo "$min" | tr '*' 'X')
+	hour=$(echo "$hour" | tr '*' 'X')
+	dom=$(echo "$dom" | tr '*' 'X')
+	mon=$(echo "$mon" | tr '*' 'X')
+	dow=$(echo "$dow" | tr '*' 'X')
 }
 
 echo ""
@@ -71,6 +62,8 @@ echo ""
 
 errors=0
 exitstatus=0
+sourceline="$@"
+
 
 #### Main
 
@@ -80,30 +73,36 @@ echo "Validating cron schedule \"$@\" ..."
 
 # check that we received exactly 1 argument
 if [ $# -ne 1 ] ; then
-    echo ""
-    echo "Use double braces around your arguments!" >&2
-    echo "Example: \"0 4 * * 6\"" >&2
-    echo ""
-    exit 1
+	echo ""
+	echo "Use double braces around your arguments!" >&2
+	echo "You entered: \"$sourceline\"" >&2
+	echo "Valid example: \"0 4 * * 6\"" >&2
+	echo ""
+	exit 1
 fi
 
-read min hour dom mon dow extra <<< "$@"
+read min hour dom mon dow extra <<< "$sourceline"
 
 if [ ! -z "$extra" ]; then
-    echo ""
-    echo "Error: Too many arguments! I don't know what to do with \"$extra\"!"
-    echo "Example: \"0 4 * * 6\"" >&2
-    echo ""
-    exit 1;
+	echo ""
+	echo "Error: Too many arguments! I don't know what to do with \"$extra\"!" >&2
+	echo "Use double braces around your arguments!" >&2
+	echo "You entered: \"$sourceline\"" >&2
+	echo "Valid example: \"0 4 * * 6\"" >&2
+	echo ""
+	exit 1;
 fi
 
-if [ -z "$min" ]; then
-# if there is nothing to check
-    echo ""
-    echo "This script requires crontab schedule line as an argument!" >&2
-    echo "Example: \"0 4 * * 6\"" >&2
-    echo ""
-    exit 1
+if [ -z "$min" ] || [ -z "$hour" ] || [ -z "$dom" ] || [ -z "$mon" ] || [ -z "$dow" ]; then
+# if some arguments are missing
+	echo ""
+	echo "Not enough arguments!"
+	echo "This script requires crontab schedule line as an argument!" >&2
+	echo "Use double braces around your arguments!" >&2
+	echo "You entered: \"$sourceline\"" >&2
+	echo "Valid example: \"0 4 * * 6\"" >&2
+	echo ""
+	exit 1
 fi
 
 
@@ -113,59 +112,61 @@ fi
 fixvars
 
 # minute check
-  for minslice in $(echo "$min" | sed 's/[,-]/ /g') ; do
-    if ! validNum $minslice 60 ; then
-      echo "Invalid minute value \"$minslice\""
-      errors="$(( $errors + 1 ))"
-    fi
-  done
+for minslice in $(echo "$min" | sed 's/[,-]/ /g') ; do
+	if ! validNum $minslice 60 ; then
+		echo "Invalid minute value \"$minslice\"" >&2
+		errors="$(( $errors + 1 ))"
+	fi
+done
 
 # hour check
 
-  for hrslice in $(echo "$hour" | sed 's/[,-]/ /g') ; do
-    if ! validNum $hrslice 24 ; then
-      echo "Invalid hour value \"$hrslice\"" 
-      errors="$(( $errors + 1 ))"
-    fi
-  done
+for hrslice in $(echo "$hour" | sed 's/[,-]/ /g') ; do
+	if ! validNum $hrslice 24 ; then
+		echo "Invalid hour value \"$hrslice\"" >&2
+		errors="$(( $errors + 1 ))"
+	fi
+done
 
 # day of month check
 
-  for domslice in $(echo $dom | sed 's/[,-]/ /g') ; do
-    if ! validNum $domslice 31 ; then
-      echo "Invalid day of month value \"$domslice\""
-      errors="$(( $errors + 1 ))"
-    fi
-  done
+for domslice in $(echo $dom | sed 's/[,-]/ /g') ; do
+	if ! validNum $domslice 31 ; then
+		echo "Invalid day of month value \"$domslice\"" >&2
+		errors="$(( $errors + 1 ))"
+	fi
+done
 
 # month check
 
-  for monslice in $(echo "$mon" | sed 's/[,-]/ /g') ; do
-    if ! validNum $monslice 12 ; then
-      if ! validMon "$monslice" ; then
-		echo "Invalid month value \"$monslice\""
-		errors="$(( $errors + 1 ))"
-      fi
-    fi
-  done
+for monslice in $(echo "$mon" | sed 's/[,-]/ /g') ; do
+	if ! validNum $monslice 12 ; then
+		if ! validMon "$monslice" ; then
+			echo "Invalid month value \"$monslice\"" >&2
+			errors="$(( $errors + 1 ))"
+		fi
+	fi
+done
 
 # day of week check
 
 for dowslice in $(echo "$dow" | sed 's/[,-]/ /g') ; do
-    if ! validNum $dowslice 7 ; then
-      if ! validDay $dowslice ; then
-		echo "Invalid day of week value \"$dowslice\""
-		errors="$(( $errors + 1 ))"
-      fi
-    fi
-  done
+	if ! validNum $dowslice 7 ; then
+		if ! validDay $dowslice ; then
+			echo "Invalid day of week value \"$dowslice\"" >&2
+			errors="$(( $errors + 1 ))"
+		fi
+	fi
+done
 
-  if [ $errors -gt 0 ] ; then
-    exitstatus=1
-    echo "$sourceline"
-  fi
-
-echo "Successfully validated cron schedule."
+if [ $errors -gt 0 ] ; then
+	exitstatus=1
+	echo ""
+	echo "You entered: \"$sourceline\"" >&2
+	echo "Valid example: \"0 4 * * 6\"" >&2
+else
+	echo "Successfully validated cron schedule."
+fi
 echo ""
 
 exit $exitstatus
