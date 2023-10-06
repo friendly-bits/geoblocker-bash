58903a786eeff452917f9141b008861a73fd0c68
blunderful_scripts <2wtangerine@gmail.com>
2023-10-06 21:13:21 +0300

    check_ip_in_ripe.sh: - shortened the description - improved the output of the usage() function - improved validation of the arguments - changed some variable names to comply with the convention used in other scripts - code cleanup - implemented multiple retries - implemented better error handling and better lists validation - improved console output messages

--- check_ip_in_ripe.sh
+++ check_ip_in_ripe.sh
@@ -3,19 +3,12 @@
 # check_ip_in_ripe.sh
 
 # Checks whether a given IP address belongs to a subnet found in RIPE's records for a given country.
+## For RIPE API, see https://stat.ripe.net/docs/data_api
 
 # Based on a prior script by mivk, called get-ripe-ips.
 
-# Fetches ipv4 subnets for a given country from RIPE
-## See https://stat.ripe.net/docs/data_api
-# Parses and compiles them into a plain list, and saves to a temporary file
-# Checks whether the user-specified IP adress belongs to one of the subnets in the list fetched from RIPE
-#
-# Requires jq - JSON processor, and grepcidr - utility that filters IPv4 and IPv6 addresses matching CIDR patterns.
-## On Debian and derivatives, if you are missing jq then install both with this command:
-## apt install jq grepcidr
-
 me=$(basename "$0")
+
 
 #### USAGE
 
@@ -23,17 +16,15 @@ usage() {
     cat <<EOF
 
 $me
-- Fetches ipv4 subnets for a given country from RIPE
-- Parses and compiles them into a plain list, and saves to a temporary file
-- Checks whether the specified IP adress belongs to one of the subnets in the list fetched from RIPE
+- Checks whether the specified IP adress belongs to one of the subnets in the list fetched from RIPE for a given country code.
 
 Requires "jq" and "grepcidr" utilities. If you are on Debian or derivatives, install both with "apt install jq grepcidr".
 
-Usage: $me -c country -i ip [-d] [-h]
+Usage: $me -c <country> -i <ip> [-d] [-h]
 
 Options:
     -c tld    : tld/country code
-    -i ip     : ipv4 address you want to check
+    -i ip     : ipv4 address to check
 
     -d        : Debug
     -h        : This help
@@ -41,33 +32,40 @@ Options:
 EOF
 }
 
+
 #### Functions
 
 die() {
-	echo "$@" 1>&2
-	echo ""
+	echo "$@" >&2
+	echo
 	exit 1
 }
 
 
 #### Parse arguments
 
-while getopts "c:i:dh" opt; do
+while getopts ":c:i:dh" opt; do
 	case $opt in
-	c) country=$OPTARG;;
+	c) tld=$OPTARG;;
 	i) userip=$OPTARG;;
 	d) debug=true;;
 	h) usage; exit 0;;
-	\?) usage; exit 1;;
+	\?) usage; die "Unknown option: '$OPTARG'." ;;
 	esac
 done
 shift $((OPTIND -1))
 
+[ "$*" != "" ] && {
+	usage
+	die "Error in arguments. First unrecognized argument: '$1'."
+}
+
 
 #### Initialize variables
 ripe_url="https://stat.ripe.net/data/country-resource-list/data.json?v4_format=prefix&resource="
-url="$ripe_url$country"
-min_subnets_num="100"
+url="$ripe_url$tld"
+## only parsing the ipv4 section at this time
+family="ipv4"
 
 # using Perl regex syntax because grep is faster with it than with native grep syntax
 # regex compiled from 2 suggestions found here:
@@ -76,154 +74,196 @@ ip_regex='^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}'
 mask_regex='(/([01]?\d\d?|2[0-4]\d|25[0-5]))$'
 subnet_regex="${ip_regex}${mask_regex}"
 
+curl_command="curl --retry 8 --fail-early --connect-timeout 7 --progress-bar"
+wget_command="wget --tries=8 --timeout=7 --show-progress"
+
+fetch_retries=3
+
 
 #### Checks
 
-if [ -z "$country" ]; then
+if [ -z "$tld" ]; then
 	usage
-	echo ""
-	err="Specify country with \"-c <country>\"!"
-	die "$err"
+	echo
+	die "Specify country with \"-c <country>\"!"
 fi
 
 # make sure that we have an ip address to check
 if [ -z "$userip" ]; then
 	usage
-	err="Specify the ip address you want to check with \"-i <ip>\"!"
-	echo ""
-	die "$err"
+	echo
+	die "Specify the ip address you want to check with \"-i <ip>\"!"
 fi
 
-# use curl or wget, depending on which one we find
-if hash curl 2>/dev/null; then
-	curl_or_wget="curl -s --retry 4 --fail-early --connect-timeout 7"
-elif hash wget 2>/dev/null; then
-	curl_or_wget="wget --tries=4 --timeout=7 -qO-"
-fi
+# check for curl
+command -v "curl" &> /dev/null && curl_exists="true"
 
-if [ -z "$curl_or_wget" ]; then
-	err="Error: Neither curl nor wget found. Cannot download data. Exiting."
-	die "$err"
-fi
+# check for wget
+command -v "wget" &> /dev/null && wget_exists="true"
+
+[[ ! "$curl_exists" && ! "$wget_exists" ]] && die 1 "Error: Neither curl nor wget found."
 
 # check that we have jq
 if ! command -v jq &> /dev/null; then
-	err="Error: Cannot find the jq Json processor. Install it with 'apt install jq' or similar. Exiting"
-	die "$err"
+	die "Error: Cannot find the jq Json processor. Install it with 'apt install jq' or similar. Exiting"
 fi
 
 # check that we have grepcidr
 if ! command -v grepcidr &> /dev/null; then
-	err="Error: Cannot find grepcidr. Install it with 'apt install grepcidr' or similar. Exiting"
-	die "$err"
+	die "Error: Cannot find grepcidr. Install it with 'apt install grepcidr' or similar. Exiting"
 fi
 
 
 #### Main
 
-echo ""
-
+echo
 
 # validate the specified ip address
 validated_ipv4=$(echo "$userip" | grep -P "$ip_regex\$")
 
 # if $validated_ipv4 is empty then validation failed
 if [ -z "$validated_ipv4" ]; then
-	err="\"$userip\" does not appear to be a valid ipv4 address. Exiting."
-	echo ""
-	die "$err"
+	echo
+	die "Error: '$userip' does not appear to be a valid ipv4 address. Exiting."
 fi
 
 fetched_file=$(mktemp "/tmp/fetched-$country-XXXX.json")
 
-echo -n "Fetching iplist from RIPE... "
-[ $debug ] && echo "Debug: Trying: $curl_or_wget '$url'" >&2
-
-$curl_or_wget "$url" > "$fetched_file"
-rv=$?
-
-if [ $rv -ne 0 ]; then
-	echo "Failed."
-	err="Error $rv trying to run $curl_or_wget $url. Exiting."
-	rm "$fetched_file" &>/dev/null
-	die "$err"
+if [ "$wget_exists" ]; then
+	fetch_command="$wget_command $url -O $fetched_file"
+elif [ "$curl_exists" ]; then
+	fetch_command="$curl_command $url -o $fetched_file"
 fi
 
+# reset variables
+retry_cnt=0
+tld_status="unknown"
+validated_subnets_cnt=0
+failed_subnets_cnt=0
+parsed_subnets_cnt=0
 
-status=$(jq -r '.status' "$fetched_file")
-if [ ! "$status" = "ok" ]; then
-	ripe_msg=$(jq -r -c '.messages' "$fetched_file")
-	echo "Failed."
-	echo "RIPE message: '$ripe_msg'."
-	echo "Requested url was: '$url'"
-	err="Error: could not fetch ip list from RIPE. Exiting"
-	rm "$fetched_file" &>/dev/null
-	die "$err"
-fi
+echo -e "Fetching subnets list for country '$tld'...\n"
 
-echo "Success."
+# Make $fetch_retries attempts to fetch the list (or until successful fetch and no validation errors)
+while true; do
+	retry_cnt=$(( retry_cnt + 1 ))
 
-## only parsing the ipv4 section at this time
-family="ipv4"
+	$fetch_command; rv=$?
 
-parsed_file=$(mktemp "/tmp/parsed-$country-XXXX.list")
-validated_file=$(mktemp "/tmp/validated-$country-XXXX.list")
-
-echo -n "Parsing downloaded subnets... "
-jq -r ".data.resources.$family | .[]" "$fetched_file" > "$parsed_file"
-
-parsed_subnet_cnt=$(wc -l < "$parsed_file")
-if [ "$parsed_subnet_cnt" -ge "$min_subnets_num" ]; then
-	echo "Success."
-else
-	err="Error: parsed subnets count $parsed_subnet_cnt is less than $min_subnets_num. Probably a download error. Exiting."
-	rm "$parsed_file" &>/dev/null
-	rm "$validated_file" &>/dev/null
-	rm "$fetched_file"
-	die "$err"
-fi
-
-echo -n "Validating downloaded subnets... "
-grep -P "$subnet_regex" "$parsed_file" > "$validated_file"
-
-validated_subnet_cnt=$(wc -l < "$validated_file")
-
-errorcount=$((parsed_subnet_cnt - validated_subnet_cnt))
-
-if [ $errorcount -ne 0 ]; then
-	echo "Issues found."
-	echo "Warning: $errorcount subnets failed validation." >&2
-	echo "Invalid subnets removed from the list." >&2
-else
-	if [ "$validated_subnet_cnt" -ge "$min_subnets_num" ]; then
-		echo "Success."
+	if [ $rv -ne 0 ]; then
+		rm "$fetched_file" &>/dev/null
+		echo "Error when running '$fetch_command'."
+		tld_status="failed"
+		[ "$retry_cnt" -ge "$fetch_retries" ] && break
 	else
-		err="Error: validated subnets count is less than $min_subnets_num. Probably a download error. Exiting."
-		rm "$parsed_file" &>/dev/null
-		rm "$validated_file" &>/dev/null
-		rm "$fetched_file"
-		die "$err"
+		status=$(jq -r '.status' "$fetched_file")
+		if [[ ! "$status" = "ok" || ! -s "$fetched_file" ]]; then
+			echo "Fetch failed." >&2
+			ripe_msg=$(jq -r -c '.messages' "$fetched_file")
+			[ "$debug"] && echo -e "RIPE replied with message: '$ripe_msg'\n" >&2
+			rm "$fetched_file" &>/dev/null
+			tld_status="failed"
+			[ "$retry_cnt" -ge "$fetch_retries" ] && break
+		else
+			echo "Fetch successful."
+
+			parsed_file=$(mktemp "/tmp/parsed-$tld-XXXX.list")
+
+			# Parse the fetched file
+			echo -n "Parsing... "
+			jq -r ".data.resources.$family | .[]" "$fetched_file" > "$parsed_file"; rv=$?
+			rm "$fetched_file" &>/dev/null
+
+			if [ "$rv" -ne 0 ]; then
+				rm "$parsed_file" &>/dev/null
+				tld_status="failed"
+				echo "Error: failed to parse the fetched file for country '$tld'." >&2
+				[ "$retry_cnt" -ge "$fetch_retries" ] && break
+			else
+				echo "Ok."
+				# Validate the parsed file
+				parsed_subnets_cnt=$(wc -l < "$parsed_file")
+
+				echo -n "Validating... "
+				validated_file=$(mktemp "/tmp/validated-$tld-XXXX.list")
+				grep -P "$subnet_regex" "$parsed_file" > "$validated_file"
+				rm "$parsed_file" &>/dev/null
+
+				validated_subnets_cnt=$(wc -l < "$validated_file")
+
+				failed_subnets_cnt=$(( parsed_subnets_cnt - validated_subnets_cnt ))
+				if [ "$failed_subnets_cnt" -gt 0 ]; then
+					echo "Note: Fetch attempt $retry_cnt: $failed_subnets_cnt subnets failed validation." >&2
+					[ "$retry_cnt" -ge "$fetch_retries" ] && { tld_status="partial"; break; }
+					rm "$validated_file" &>/dev/null
+				else
+					echo "Ok."
+					tld_status="Ok"
+					break
+				fi
+			fi
+		fi
 	fi
+	echo -e "Retrying fetch...\n" >&2
+done
+
+echo
+
+if [[ "$tld_status" = "Ok" && "$validated_subnets_cnt" -eq 0 ]]; then
+	rm "$validated_file" &>/dev/null
+	echo "Error: validated 0 subnets for country code '$tld'. Perhaps the country code is incorrect?" >&2
+	tld_status="failed"
 fi
 
-echo "Total validated subnets: $validated_subnet_cnt"
-echo ""
+# If we have a partial list, decide whether to consider it Ok or not
+if [ "$tld_status" = "partial" ]; then
+	echo "Warning: out of $parsed_subnets_cnt, $failed_subnets_cnt subnets for country '$tld' failed validation." >&2
+	echo "Invalid subnets removed from the list." >&2
 
-echo "Checking $userip..."
-
-echo "$userip" | grepcidr -f "$validated_file" &>/dev/null; rv=$?
-
-if [ $rv -eq 0 ]; then
-	echo "Result: $userip *BELONGS* to a subnet in RIPE's list for country \"$country\"."
-else
-	echo "Result: $userip *DOES NOT BELONG* to a subnet in RIPE's list for country \"$country\"."
+	echo -e "\n\nWarning: Ip list for country '$tld' has been fetched but may be incomplete."
+	echo "Use the incomplete list anyway?"
+	while true; do
+		read -p "(Y/N) " -n 1 -r
+		if [[ "$REPLY" =~ ^[Yy]$ ]]; then tld_status="Ok"; break
+		elif [[ "$REPLY" =~ ^[Nn]$ ]]; then
+			rm "$validated_file" &>/dev/null
+			tld_status="failed"
+		else echo -e "\nPlease press 'y/n'.\n"
+		fi
+	done
 fi
-echo ""
 
-# clean up temp files
-rm "$fetched_file"
-rm "$parsed_file"
-rm "$validated_file"
-echo "Done."
-echo ""
-exit $rv
+case "$tld_status" in
+	Ok ) ;;
+	failed ) rm "$validated_file" &>/dev/null; die "Failed to check the ip address in RIPE for country '$tld'." ;;
+	* ) rm "$validated_file" &>/dev/null; die "Error: unrecognized \$tld_status '$tld_status'. Failed to check the ip address in RIPE for country '$tld'." ;;
+esac
+
+echo -e "\nValidated subnets count for country '$tld': $validated_subnets_cnt.\n"
+
+echo -e "Checking $userip...\n"
+
+filtered_ip="$(echo "$userip" | grepcidr -f "$validated_file")"; rv=$?
+
+# adapt the results for use in truth table
+[ "$rv" -ne 0 ] && rv1=1 || rv1=0
+[ "$filtered_ip" = "" ] && rv2=10 || rv2=0
+
+red_code='\033[0;31m'
+green_code='\033[0;32m'
+no_color='\033[0m'
+
+# check the result based on a truth table for $rv1, $rv2
+truth_table_result=$(( 2#$rv1 + 2#$rv2))
+
+case "$truth_table_result" in
+	0) echo -e "Result: '$userip' ${green_code}*BELONGS*${no_color} to a subnet in RIPE's list for country '$tld'." ;;
+	1) echo -e "${red_code}Error${no_color}: grepcidr reported an error but returned a non-empty '\$filtered_ip'. Something is wrong in the script code." ;;
+	2) echo -e "${red_code}Error${no_color}: grepcidr didn't report any error but returned an empty '\$filtered_ip'. Something is wrong in the script code." ;;
+	3) echo -e "Result: '$userip' ${red_code}*DOES NOT BELONG*${no_color} to a subnet in RIPE's list for country '$tld'." ;;
+	*) echo -e "${red_code}Error${no_color}: unexpected \$truth_table_result: '$truth_table_result'." ;;
+esac
+
+rm "$validated_file" &>/dev/null
+
+exit "$truth_table_result"
