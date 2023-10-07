b4d42f7edc45990fde7431f44d4a3d1f7c705338
blunderful_scripts <2wtangerine@gmail.com>
2023-10-08 00:02:07 +0300

    check_ip_in_ripe.sh: - implemented support for checking multiple ip addresses at once - updated the description and the usage() function - check for awk and grep on initialization, just in case - implemented more reliable processing of grep and grepcidr results (than just relying on the return value), moved existing related code to new function process_grep_results() - changed names of some variables - reduced connection timeout time from 7s back to 5s - improved some error messages - added some code comments - added colors to final output - exit with a binary return value

--- check_ip_in_ripe.sh
+++ check_ip_in_ripe.sh
@@ -2,7 +2,8 @@
 
 # check_ip_in_ripe.sh
 
-# Checks whether a given IP address belongs to a subnet found in RIPE's records for a given country.
+# For each of the specified ip addresses, checks whether it belongs to one of the subnets in the list fetched from RIPE for a given country code.
+
 ## For RIPE API, see https://stat.ripe.net/docs/data_api
 
 # Based on a prior script by mivk, called get-ripe-ips.
@@ -16,18 +17,20 @@ usage() {
     cat <<EOF
 
 $me
-- Checks whether the specified IP adress belongs to one of the subnets in the list fetched from RIPE for a given country code.
+    For each of the specified ip addresses, checks whether it belongs to one of the subnets in the list fetched from RIPE for a given country code.
 
-Requires "jq" and "grepcidr" utilities. If you are on Debian or derivatives, install both with "apt install jq grepcidr".
+Requires "jq" and "grepcidr" utilities.
+Requires GNU 'grep' and 'awk' utilities (most Linux distributions include these by default).
 
-Usage: $me -c <country> -i <ip> [-d] [-h]
+Usage: $me -c <country_code> -i <"ip [ip ... ip]"> [-d] [-h]
 
 Options:
-    -c tld    : tld/country code
-    -i ip     : ipv4 address to check
+    -c <country_code>    : tld/country code
+    -i <"ip_addresses">  : ipv4 addresses to check
+                           - if specifying multiple addresses, use double quotes
 
-    -d        : Debug
-    -h        : This help
+    -d                   : Debug
+    -h                   : This help
 
 EOF
 }
@@ -36,9 +39,26 @@ EOF
 #### Functions
 
 die() {
-	echo "$@" >&2
-	echo
+	echo -e "$@\n" >&2
 	exit 1
+}
+
+process_grep_results() {
+# takes grep return value $1 and grep output string $2,
+# converts these results into a truth table,
+# then calculates the validation result based on the truth table sum
+# the idea is to cross-reference both values in order to avoid erroneous validation results
+
+	grep_rv="$1"
+	grep_output="$2"
+
+	# convert 'grep return value' and 'grep output value' resulting value into truth table inputs
+	[ "$grep_rv" -ne 0 ] && rv1=1 || rv1=0
+	[ "$grep_output" = "" ] && rv2=10 || rv2=0
+
+	# calculate the truth table sum
+	truth_table_result=$(( 2#$rv1 + 2#$rv2))
+	return "$truth_table_result"
 }
 
 
@@ -47,7 +67,7 @@ die() {
 while getopts ":c:i:dh" opt; do
 	case $opt in
 	c) tld=$OPTARG;;
-	i) userip=$OPTARG;;
+	i) arg_ipv4s=$OPTARG;;
 	d) debug=true;;
 	h) usage; exit 0;;
 	\?) usage; die "Unknown option: '$OPTARG'." ;;
@@ -62,6 +82,11 @@ shift $((OPTIND -1))
 
 
 #### Initialize variables
+
+red='\033[0;31m'
+green='\033[0;32m'
+no_color='\033[0m'
+
 ripe_url="https://stat.ripe.net/data/country-resource-list/data.json?v4_format=prefix&resource="
 url="$ripe_url$tld"
 ## only parsing the ipv4 section at this time
@@ -70,14 +95,21 @@ family="ipv4"
 # using Perl regex syntax because grep is faster with it than with native grep syntax
 # regex compiled from 2 suggestions found here:
 # https://stackoverflow.com/questions/5284147/validating-ipv4-addresses-with-regexp
-ip_regex='^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}'
+ipv4_regex='^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}'
 mask_regex='(/([01]?\d\d?|2[0-4]\d|25[0-5]))$'
-subnet_regex="${ip_regex}${mask_regex}"
+subnet_regex="${ipv4_regex}${mask_regex}"
 
-curl_command="curl --retry 8 --fail-early --connect-timeout 7 --progress-bar"
-wget_command="wget --tries=8 --timeout=7 --show-progress"
+curl_command="curl --retry 8 --fail-early --connect-timeout 5 --progress-bar"
+wget_command="wget --tries=8 --timeout=5 --show-progress"
 
 fetch_retries=3
+
+validated_ipv4s=""
+invalid_ipv4s=""
+ip_check_rv=0
+
+# trim extra whitespaces
+arg_ipv4s="$(awk '{$1=$1};1' <<< "$arg_ipv4s")"
 
 
 #### Checks
@@ -88,11 +120,21 @@ if [ -z "$tld" ]; then
 	die "Specify country with \"-c <country>\"!"
 fi
 
-# make sure that we have an ip address to check
-if [ -z "$userip" ]; then
+# make sure that we have ip addresses to check
+if [ -z "$arg_ipv4s" ]; then
 	usage
 	echo
-	die "Specify the ip address you want to check with \"-i <ip>\"!"
+	die "Specify the ip addresses you want to check with \"-i <ip>\"!"
+fi
+
+# check for awk
+if ! command -v awk &> /dev/null; then
+	die "Error: Cannot find awk. Install it with 'sudo apt install gawk' or similar."
+fi
+
+# check for grep
+if ! command -v grep &> /dev/null; then
+	die "Error: Cannot find grep. Install it with 'sudo apt install grep' or similar."
 fi
 
 # check for curl
@@ -101,16 +143,16 @@ command -v "curl" &> /dev/null && curl_exists="true"
 # check for wget
 command -v "wget" &> /dev/null && wget_exists="true"
 
-[[ ! "$curl_exists" && ! "$wget_exists" ]] && die 1 "Error: Neither curl nor wget found."
+[[ ! "$curl_exists" && ! "$wget_exists" ]] && die "Error: Neither curl nor wget found."
 
-# check that we have jq
+# check for jq
 if ! command -v jq &> /dev/null; then
-	die "Error: Cannot find the jq Json processor. Install it with 'apt install jq' or similar. Exiting"
+	die "Error: Cannot find the jq Json processor. Install it with 'sudo apt install jq' or similar."
 fi
 
-# check that we have grepcidr
+# check for grepcidr
 if ! command -v grepcidr &> /dev/null; then
-	die "Error: Cannot find grepcidr. Install it with 'apt install grepcidr' or similar. Exiting"
+	die "Error: Cannot find grepcidr. Install it with 'sudo apt install grepcidr' or similar."
 fi
 
 
@@ -118,13 +160,31 @@ fi
 
 echo
 
-# validate the specified ip address
-validated_ipv4=$(echo "$userip" | grep -P "$ip_regex\$")
+for arg_ipv4 in $arg_ipv4s; do
+	# validate the ip address by grepping it with the pre-defined validation regex
+	validated_ipv4=$(echo "$arg_ipv4" | grep -P "$ipv4_regex\$"); rv=$?
 
-# if $validated_ipv4 is empty then validation failed
-if [ -z "$validated_ipv4" ]; then
+	# process grep results
+	process_grep_results "$rv" "$validated_ipv4"; true_grep_rv=$?
+
+	case "$true_grep_rv" in
+		0) validated_ipv4s="$validated_ipv4s $validated_ipv4" ;;
+		1) die "${red}Error${no_color}: grep reported an error but returned a non-empty '\$validated_ipv4'. Something is wrong." ;;
+		2) die "${red}Error${no_color}: grep didn't report any error but returned an empty '\$validated_ipv4'. Something is wrong." ;;
+		3) echo -e "\nError: '$arg_ipv4' does not appear to be a valid ipv4 address."
+			invalid_ipv4s="$invalid_ipv4s $arg_ipv4" ;;
+		*) die "${red}Error${no_color}: unexpected \$true_grep_rv: '$true_grep_rv'. Something is wrong." ;;
+	esac
+done
+
+# trim extra whitespaces
+validated_ipv4s="$(awk '{$1=$1};1' <<< "$validated_ipv4s")"
+invalid_ipv4s="$(awk '{$1=$1};1' <<< "$invalid_ipv4s")"
+
+# if $validated_ipv4 is empty then validation of all ip's failed
+if [ -z "$validated_ipv4s" ]; then
 	echo
-	die "Error: '$userip' does not appear to be a valid ipv4 address. Exiting."
+	die "Error: all ipv4 addresses failed validation."
 fi
 
 fetched_file=$(mktemp "/tmp/fetched-$country-XXXX.json")
@@ -136,7 +196,7 @@ elif [ "$curl_exists" ]; then
 fi
 
 # reset variables
-retry_cnt=0
+retry_cnt=1
 tld_status="unknown"
 validated_subnets_cnt=0
 failed_subnets_cnt=0
@@ -146,13 +206,12 @@ echo -e "Fetching subnets list for country '$tld'...\n"
 
 # Make $fetch_retries attempts to fetch the list (or until successful fetch and no validation errors)
 while true; do
-	retry_cnt=$(( retry_cnt + 1 ))
-
 	$fetch_command; rv=$?
 
 	if [ $rv -ne 0 ]; then
 		rm "$fetched_file" &>/dev/null
-		echo "Error when running '$fetch_command'."
+		echo "Failed to fetch subnets list for country '$tld'." >&2
+		[ "$debug" ] && echo -e "\nDebug: Error $rv when running '$fetch_command'." >&2
 		tld_status="failed"
 		[ "$retry_cnt" -ge "$fetch_retries" ] && break
 	else
@@ -160,7 +219,7 @@ while true; do
 		if [[ ! "$status" = "ok" || ! -s "$fetched_file" ]]; then
 			echo "Fetch failed." >&2
 			ripe_msg=$(jq -r -c '.messages' "$fetched_file")
-			[ "$debug"] && echo -e "RIPE replied with message: '$ripe_msg'\n" >&2
+			[ "$debug"] && echo -e "Debug: RIPE replied with message: '$ripe_msg'\n" >&2
 			rm "$fetched_file" &>/dev/null
 			tld_status="failed"
 			[ "$retry_cnt" -ge "$fetch_retries" ] && break
@@ -204,7 +263,8 @@ while true; do
 			fi
 		fi
 	fi
-	echo -e "Retrying fetch...\n" >&2
+	retry_cnt=$(( retry_cnt + 1 ))
+	echo -e "\nRetrying fetch (attempt $retry_cnt of $fetch_retries)...\n" >&2
 done
 
 echo
@@ -241,29 +301,37 @@ esac
 
 echo -e "\nValidated subnets count for country '$tld': $validated_subnets_cnt.\n"
 
-echo -e "Checking $userip...\n"
+for validated_ipv4 in $validated_ipv4s; do
+	filtered_ipv4="$(echo "$validated_ipv4" | grepcidr -f "$validated_file")"; rv=$?
 
-filtered_ip="$(echo "$userip" | grepcidr -f "$validated_file")"; rv=$?
+	# validate grep results
+	process_grep_results "$rv" "$filtered_ipv4"; true_grep_rv=$?
 
-# adapt the results for use in truth table
-[ "$rv" -ne 0 ] && rv1=1 || rv1=0
-[ "$filtered_ip" = "" ] && rv2=10 || rv2=0
+	case "$true_grep_rv" in
+		0) echo -e "Result: '$validated_ipv4' ${green}*BELONGS*${no_color} to a subnet in RIPE's list for country '$tld'." ;;
+		1) echo -e "${red}Error${no_color}: grepcidr reported an error but returned a non-empty '\$filtered_ipv4'. Something is wrong."
+			fatal_error="true" ;;
+		2) echo -e "${red}Error${no_color}: grepcidr didn't report any error but returned an empty '\$filtered_ipv4'. Something is wrong."
+			fatal_error="true" ;;
+		3) echo -e "Result: '$validated_ipv4' ${red}*DOES NOT BELONG*${no_color} to a subnet in RIPE's list for country '$tld'." ;;
+		*) echo -e "${red}Error${no_color}: unexpected \$truth_table_result: '$truth_table_result'. Something is wrong."
+			fatal_error="true" ;;
+	esac
 
-red_code='\033[0;31m'
-green_code='\033[0;32m'
-no_color='\033[0m'
+	[ "$grepcidr_error" = true ] && { rm "$validated_file" &>/dev/null; die "Failed to process grepcidr results."; }
 
-# check the result based on a truth table for $rv1, $rv2
-truth_table_result=$(( 2#$rv1 + 2#$rv2))
+	# increment the return value if matching didn't succeed for any reason
+	[ "$truth_table_result" -ne 0 ] && ip_check_rv=$(( ip_check_rv + 1 ))
+done
 
-case "$truth_table_result" in
-	0) echo -e "Result: '$userip' ${green_code}*BELONGS*${no_color} to a subnet in RIPE's list for country '$tld'." ;;
-	1) echo -e "${red_code}Error${no_color}: grepcidr reported an error but returned a non-empty '\$filtered_ip'. Something is wrong in the script code." ;;
-	2) echo -e "${red_code}Error${no_color}: grepcidr didn't report any error but returned an empty '\$filtered_ip'. Something is wrong in the script code." ;;
-	3) echo -e "Result: '$userip' ${red_code}*DOES NOT BELONG*${no_color} to a subnet in RIPE's list for country '$tld'." ;;
-	*) echo -e "${red_code}Error${no_color}: unexpected \$truth_table_result: '$truth_table_result'." ;;
-esac
+
+if [ -n "$invalid_ipv4s" ]; then
+	echo -e "${red}Invalid${no_color} ipv4 addresses: '$invalid_ipv4s'"
+	ip_check_rv=$(( ip_check_rv + 1 ))
+fi
 
 rm "$validated_file" &>/dev/null
 
-exit "$truth_table_result"
+echo
+
+[ "$ip_check_rv" -gt 0 ] && exit 1 || exit 0
